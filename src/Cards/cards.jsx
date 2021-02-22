64a61625129124206141efc26d4d6d5a5d3e2eeb
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-02-22 18:55:33 +0500

    refactor: add array

--- src/Cards/cards.jsx
+++ src/Cards/cards.jsx
@@ -2,53 +2,15 @@ import React from 'react';
 import './cards.css';
 
 const Cards = () => {
-  // let cardArray = [{ name: 'club', img: 'img/cards/clubs_2.png' }];
-  return (
-    <div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
+  const arr = [0, 1, 2, 3, 4, 5];
 
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-      </div>
+  const cards = arr.map(item => (
+    <div className='memory-card'>
+      <img className='front-face' src={`img/cards/clubs_${item}.png`} alt='club' />
     </div>
-  );
+  ));
+
+  return cards;
 };
 
 export default Cards;
