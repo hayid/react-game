c88ad2dcbb351fe0c6a90e5a61c25c0a9b596603
hayit08 <72305423+hayit08@users.noreply.github.com>
2021-02-22 15:16:19 +0500

    refactor: change style of cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,11 @@
 import React from 'react';
-
+import Cards from './Cards/cards';
 import './App.css';
 
 const App = () => {
   return (
     <div className='memory-game'>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
-      <div className='memory-card'>
-        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
-        <img className='back-face' src='img/logo.png' alt='JS Badge' />
-      </div>
+      <Cards />
     </div>
   );
 };
---
+++ src/Cards/cards.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import './cards.css';
+
+const Cards = () => {
+  return (
+    <div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+      <div className='memory-card'>
+        <img className='front-face' src='img/cards/clubs_2.png' alt='club' />
+        <img className='back-face' src='img/logo.png' alt='JS Badge' />
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
